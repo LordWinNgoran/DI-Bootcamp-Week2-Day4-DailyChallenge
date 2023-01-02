1548533597094a4467d58ecf5446b97a0f2dc720
LordWinNgoran <cyrille4011@gmail.com>
2023-01-02 14:09:29 +0100

    Commented programm

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
-// Daily Challenge: Words In The Stars
+/****************************Daily Challenge: Words In The Stars***************** */
+
+/**
+ * @author: N'GORAN Kouadio Jean Cyrille
+ * @description: Words In The Stars
+ * @param: initialBottles:Number
+ * Date : 30/12/2022
+ */
+
 
 //1 Prompt the user for several words (separated by commas).
 
@@ -8,23 +16,34 @@ let userWord = prompt("Please enter several words (seperated by commas)");
 
 let userArray = userWord.split(",");
 
+
+// create var for the longest word
 let LongestWord = 0;
+
+// inside this loop we search by  loop index the length of each index and compare to LongestWord
 for (let indexUserArray = 0; indexUserArray < userArray.length; indexUserArray++) {
   let lengthWord = userArray[indexUserArray].length;
+
+
+// if the new length is great than LongestWord is become the new LongestWord
   if (lengthWord > LongestWord) {
     LongestWord = lengthWord;
   }
 
 }
 
-console.log(`* `.repeat(LongestWord));
+
+
+// we show line per line 
+console.log(`*`.repeat(LongestWord+4));
 for (let indexUserArray = 0; indexUserArray < userArray.length; indexUserArray++) {
+//3 Console.log the words one per line, in a rectangular frame as seen below.
   if (LongestWord > userArray[indexUserArray].length) {
-    console.log('* ' + userArray[indexUserArray] + ' '.repeat(LongestWord*2-userArray[indexUserArray].length-4) + "*");
+    console.log('* ' + userArray[indexUserArray] + ' '.repeat(LongestWord-userArray[indexUserArray].length+1) + "*");
   }
   else {
-    console.log('* ' + userArray[indexUserArray] + ' '.repeat(LongestWord - 4) + "*");
+    console.log('* ' + userArray[indexUserArray] + " *");
   }
 
 }
-console.log(`* `.repeat(LongestWord));
+console.log(`*`.repeat(LongestWord+4));
