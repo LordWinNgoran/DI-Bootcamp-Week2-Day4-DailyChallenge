7abd2077905d36a27a4e9440bcb59462d072b461
LordWinNgoran <cyrille4011@gmail.com>
2023-01-02 13:51:28 +0100

    Fix rigth star for all line

--- script.js
+++ script.js
@@ -3,28 +3,28 @@
 
 //1 Prompt the user for several words (separated by commas).
 
-let userWord=prompt("Please enter several words (seperated by commas)");
+let userWord = prompt("Please enter several words (seperated by commas)");
 //2 Put the words into an array.
 
-let userArray= userWord.split(",");
+let userArray = userWord.split(",");
 
-let LongestWord=0;
+let LongestWord = 0;
 for (let indexUserArray = 0; indexUserArray < userArray.length; indexUserArray++) {
-    let lengthWord=userArray[indexUserArray].length;
-      if(lengthWord>LongestWord){
-          LongestWord=lengthWord;
-      }
-  
+  let lengthWord = userArray[indexUserArray].length;
+  if (lengthWord > LongestWord) {
+    LongestWord = lengthWord;
+  }
+
 }
 
 console.log(`* `.repeat(LongestWord));
 for (let indexUserArray = 0; indexUserArray < userArray.length; indexUserArray++) {
-  if(LongestWord>userArray[indexUserArray].length){
-    console.log('* '+userArray[indexUserArray]+' '.repeat(LongestWord)+"*");
+  if (LongestWord > userArray[indexUserArray].length) {
+    console.log('* ' + userArray[indexUserArray] + ' '.repeat(LongestWord*2-userArray[indexUserArray].length-4) + "*");
   }
-  else{
-    console.log('* '+userArray[indexUserArray]+' '.repeat(LongestWord-4)+"*");
+  else {
+    console.log('* ' + userArray[indexUserArray] + ' '.repeat(LongestWord - 4) + "*");
   }
-  
+
 }
 console.log(`* `.repeat(LongestWord));
